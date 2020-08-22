387d1ec00e3de36e7364741642e81766e16d473c
Salmaan Rizvi <sar228@cornell.edu>
2020-08-21 21:42:37 -0700

    client: update to page action and show and hide popup on nytimes

--- webpack-client/src/js/background.js
+++ webpack-client/src/js/background.js
@@ -1,2 +1,25 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+import { TableSortLabel } from '@material-ui/core';
+
+var lastTabId = 0;
+
+chrome.tabs.onSelectionChanged.addListener(function(tabId) {
+  lastTabId = tabId;
+  console.log('tab changed to', tabId)
+  chrome.tabs.query({ url: 'https://www.nytimes.com/crosswords*'}, tabs => {
+    const currentTab = tabs.find(tab => tab.id === tabId)
+    if (currentTab) {
+      chrome.pageAction.show(lastTabId);
+    } else {
+      chrome.pageAction.hide(lastTabId)
+    }
+  })
+});
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  lastTabId = tabs[0].id;
+  if (tabs[0].url.indexOf('https://www.nytimes.com/crosswords') > -1) {
+    chrome.pageAction.show(lastTabId);
+  }
+});
