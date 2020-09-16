4cd202a2aa7af7cb10bb9e93dfd86c555b84077f
Salmaan Rizvi <sar228@cornell.edu>
2020-09-15 23:24:57 -0700

    client: comment out content script code

--- webpack-client/src/js/contentScript.js
+++ webpack-client/src/js/contentScript.js
@@ -3,3 +3,13 @@ script.id = '__crossword_party';
 script.src = chrome.runtime.getURL('injectMiddleware.bundle.js');
 (document.head || document.documentElement).appendChild(script);
 console.log('loaded script')
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   console.log(sender.tab ?
+//               "from a content script:" + sender.tab.url :
+//               "from the extension")
+//   if (request.type == "SYNC_GAME") {
+//     window.postMessage({ source: '__cwp', ...request })
+//     sendResponse({ received: true })
+//   }
+// })
